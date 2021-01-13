751c7818085b25b18014d664c548a35db6665ba2
dale.sakamoto <daletsakamoto@gmail.com>
2021-01-12 21:54:35 -0800

    search styling done

--- frontend/src/components/Search/index.js
+++ frontend/src/components/Search/index.js
@@ -15,12 +15,10 @@ function Search() {
   const userDesigners = useSelector(state => state.session.designers)
   const [keywordSearch, setKeywordSearch] = useState('')
   const [isLoaded, setIsLoaded] = useState(false);
-  const [isLoaded2, setIsLoaded2] = useState(false);
   let id = sessionUser.id;
 
   useEffect(() => {
     dispatch(sessionActions.searchUserDesigners(id))
-    .then(setIsLoaded2(true))
   },[])
 
   useEffect(() => {
@@ -33,7 +31,7 @@ function Search() {
     dispatch(userActions.searchDesigners(keywordSearch))
   }
 
-  return isLoaded && isLoaded2 &&(
+  return isLoaded &&(
     <div className='search-container pattern-cross-dots-lg'>
       <div className="search-bar-container">
           <h1 className='search-bar-title'>Search for a Designer</h1>
@@ -47,39 +45,57 @@ function Search() {
             <i className="fas fa-search search-magnify" />
           </button>
         </div>
-          <h2 className='search-results-title'>Results</h2>
+        <div className='search-results-title pattern-cross-dots-lg'>
+          <h2>Results</h2>
+        </div>
         {designersSearch ?
             Object.values(designersSearch).map((person, idx) => {
       return (
-        <div key={idx} className='results-container__body__local'>
-            <NavLink className='navlinks' to={`/users/${person.id}`}>
-              <div className='results-local-header'>
-                <img className='results-local-header__image' src={person.avatar} />
-                <h1 className='results-local-username'>{person.firstName} {person.lastName.slice(0, 1).toUpperCase()}.</h1>
+        <div key={idx} className='search-my-designers-individual'>
+        <NavLink className='search-my-designers-navlinks' to={`/users/${person.id}`}>
+          <div className='search-my-designers-header'>
+          <img className='search-my-designers-image' src={person.avatar} />
+            <div className='search-my-designers-name-rating'>
+              <h1 className='search-my-designers-name'>{person.firstName} {person.lastName}</h1>
+              <div className='search-my-designers-ratings-container'>
+                <img id='search-my-designers-ratings-1' src='../images/Dress.png' />
+                <img id='search-my-designers-ratings-2' src='../images/Dress.png' />
+                <img id='search-my-designers-ratings-3' src='../images/Dress.png' />
+                <img id='search-my-designers-ratings-4' src='../images/Dress.png' />
+                <img id='search-my-designers-ratings-5' src='../images/Dress.png' />
               </div>
-            </NavLink>
-            <div className='results-local-user__bio'>{person.bio}</div>
-        </div>
+            </div>
+          </div>
+        </NavLink>
+      </div>
       )
     })
           :
           null
       }
       </div>
-      <div className="search-bar-spacer"></div>
       <div className='search-my-designers-container'>
-      <h1 className='search-my-designers-header'>My Designers</h1>
+        <div className='search-my-designers-title pattern-cross-dots-lg'>
+          <h1>My Designers</h1>
+        </div>
         {userDesigners ?
             Object.values(userDesigners).map((person, idx) => {
               return (
-                <div key={idx} className='results-container__body__local'>
-                  {console.log(person)}
-                  <NavLink className='navlinks' to={`/users/${person.designerId}`}>
-                    <div className='results-local-header'>
-                      <img className='results-local-header__image' src={person.designerAvatar} />
-                      <h1 className='results-local-username'>{person.designerFirstName} { person.designerLastName }</h1>
+                <div key={idx} className='search-my-designers-individual'>
+                  <NavLink className='search-my-designers-navlinks' to={`/users/${person.designerId}`}>
+                    <div className='search-my-designers-header'>
+                      <img className='search-my-designers-image' src={person.designerAvatar} />
+                      <div className='search-my-designers-name-rating'>
+                        <h1 className='search-my-designers-name'>{person.designerFirstName} {person.designerLastName}</h1>
+                        <div className='search-my-designers-ratings-container'>
+                          <img id='search-my-designers-ratings-1' src='../images/Dress.png' />
+                          <img id='search-my-designers-ratings-2' src='../images/Dress.png' />
+                          <img id='search-my-designers-ratings-3' src='../images/Dress.png' />
+                          <img id='search-my-designers-ratings-4' src='../images/Dress.png' />
+                          <img id='search-my-designers-ratings-5' src='../images/Dress.png' />
+                        </div>
+                      </div>
                     </div>
-                    <p>{person.designerBio}</p>
                   </NavLink>
                 </div>
               )
@@ -87,6 +103,7 @@ function Search() {
           :
           null}
       </div>
+      <div className='search-margin-spacer'></div>
     </div>
   )
 }
