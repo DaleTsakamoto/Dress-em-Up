63018dbbec733fa41f0240bc962969fe84f35d3b
dale.sakamoto <daletsakamoto@gmail.com>
2021-02-19 17:14:55 -0800

    web socket setup

---
+++ frontend/src/components/Messaging/Messaging.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+import './Messaging.css';
+
+function Messaging() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  return (
+    <div>
+      <h1>Messaging</h1>
+    </div>
+  )
+}
+
+export default Messaging;
