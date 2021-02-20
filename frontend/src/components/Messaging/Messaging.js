24e5e898a8bddf5ddfeb93470ef30ff1d2f727c8
dale.sakamoto <daletsakamoto@gmail.com>
2021-02-19 21:05:25 -0800

    setting up main doc

--- frontend/src/components/Messaging/Messaging.js
+++ frontend/src/components/Messaging/Messaging.js
@@ -1,9 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import uuid from 'uuid'
 
 import './Messaging.css';
 
 function Messaging() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [username, setUsername] = useState('');
+  const [messages, setMessages] = useState('hello')
+
+  const updateUsername = (username) => {
+    setUsername(username);
+  }
+
+  const handleSendMessage = (message) => {
+    const newMessage = {
+      id: uuid(),
+      message,
+      created:new Date()
+    }
+
+    setMessages([newMessages, ...messages])
+  }
+
+  const handleLeave = () => {
+    setUsername('');
+  }
+
 
   return (
     <div>
