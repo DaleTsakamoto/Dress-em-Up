f535815302d3f1d04765c17cbae55c94241fbbbb
dale.sakamoto <daletsakamoto@gmail.com>
2021-02-22 20:29:57 -0800

    bug in backend

--- frontend/src/components/ChatRoom/index.js
+++ frontend/src/components/ChatRoom/index.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-
+import { useSelector } from 'react-redux'
 import styles from './Chatroom.css'
 
-const Chatroom = ({ messages, handleSendMessage, handleLeave }) => {
+const Chatroom = ({ messages, handleSendMessage, handleLeave, updateUsername }) => {
   const [message, setMessage] = useState('')
+  const sessionUser = useSelector(state => state.session.user);
 
   const handleOnChange = (e) => {
     setMessage(e.target.value)
@@ -24,6 +25,7 @@ const Chatroom = ({ messages, handleSendMessage, handleLeave }) => {
         <input type='text' value={message} onChange={handleOnChange} />
         <button type='button' onClick={handleSendOnClick}>Send</button>
         <button type='button' onClick={handleLeaveOnClick}>Leave</button>
+        <button type='button' onClick={() => updateUsername(sessionUser.username)}>start</button>
         <div className={styles.messages}>
           {messages.map(m => (
             <div className='message-info-ind-container'>
--- frontend/src/components/Messaging/index.js
+++ frontend/src/components/Messaging/index.js
@@ -1,27 +1,73 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { v4 as uuid } from 'uuid'
-
+import { useSelector } from 'react-redux'
 
 import Chatroom from '../ChatRoom'
 import './Messaging.css';
 
 function Messaging() {
+  // const sessionUser = useSelector(state => state.session.user);
   const [isLoaded, setIsLoaded] = useState(false);
   const [username, setUsername] = useState('');
   const [messages, setMessages] = useState([])
+  const webSocket = useRef(null);
+
+  useEffect(() => {
+    if (!username) {
+      return;
+    }
+
+    const ws = new WebSocket('ws://localhost:5000')
+
+    ws.onopen = (e) => {
+      console.log(`Connection open: ${e}`)
+    }
+
+    ws.onmessage = (e) => {
+      console.log(e)
+    }
+
+    ws.onerror = (e) => {
+      console.log(e)
+    }
+
+    ws.onclose = (e) => {
+      console.log(`Connection closed: ${e}`)
+    }
+
+    webSocket.current = ws;
+
+    return function cleanup() {
+      if (webSocket.current !== null) {
+        webSocket.current.close()
+      }
+    }
+
+  }, [username])
 
   const updateUsername = (username) => {
+    console.log(username)
     setUsername(username);
   }
 
   const handleSendMessage = (message) => {
     const newMessage = {
       id: uuid(),
+      username,
       message,
-      created:new Date()
+      created: new Date()
     }
 
-    setMessages([newMessage, ...messages])
+    const jsonNewMessage = JSON.stringify({
+      type: 'send-chat-message',
+      data: newMessage,
+    });
+
+    console.log(`Sending message ${jsonNewMessage}`)
+
+    webSocket.current.send(jsonNewMessage)
+
+    // setMessages([newMessage, ...messages])
   }
 
   const handleLeave = () => {
@@ -32,7 +78,7 @@ function Messaging() {
   return (
     <div className='messaging-container'>
       <h1>Messaging</h1>
-      <Chatroom messages={messages} handleSendMessage={handleSendMessage} handleLeave={handleLeave} />
+      <Chatroom messages={messages} handleSendMessage={handleSendMessage} handleLeave={handleLeave} updateUsername={updateUsername} />
     </div>
   )
 }
